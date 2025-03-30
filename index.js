23b0142da4c1a50a7c6c28cdf3fe2966e8a8ef4b
Beatris <beatrisraj7@gmail.com>
2025-03-31 02:16:30 +0300

    DB: Users

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ import express from 'express'
 import 'dotenv/config'
 import router from './src/routes.js'
 import mongoose from 'mongoose'
+import 'dotenv/config'
 
 const app = express()
 
 try{
-    await mongoose.connect(process.env.MONGODB_CLUSTER_URI) 
+    await mongoose.connect(process.env.MONGODB_CLUSTER_URI)
     console.log('Succssesfuly connected!');
     
 } catch(err){
