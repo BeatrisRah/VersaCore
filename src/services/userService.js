b64475c07f402098706af731b3d170131be77b7a
Beatris <beatrisraj7@gmail.com>
2025-03-31 18:42:17 +0300

    Generating token with jsonwebtoken

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,4 +1,22 @@
+import jwt from "jsonwebtoken";
 import User from "../models/User.js"
+
+async function createToken({id, username, email}){
+
+    const payload = {
+        id,
+        username,
+        email,
+    }
+    let token = null;
+
+    await jwt.sign(payload, process.env.SECRET,{ expiresIn: '1h' }, function(err, generatedToken){
+        token = generatedToken;
+        
+    } )
+
+    return token;
+}
 
 export default{
     async getAll(){
